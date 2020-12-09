85ad1a280cbfd6d5ed05d4444b4e6a4875c19ebb
Pham Viet Linh <phamvietlinh164@gmail.com>
2020-12-09 15:02:55 +0700

    change layout to Pham Viet Linh

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Viet Linh'
+const name = 'Pham Viet Linh'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
